191f701bcbf47c64520dbc86d98fa48f508a95d7
lena <lenzzzx@gmail.com>
2025-07-15 18:26:49 -0400

    fix mobile responsiveness issue

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -21,7 +21,7 @@ export const Contact = () => {
     return (
         <section id="contact" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
-                <div className='px-4 w-150'>
+                <div className='w-full max-w-xl px-4'>
                     <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-pink-500 to-yellow-100 bg-clip-text text-transparent text-center'>
                         Get in touch
                     </h2>
